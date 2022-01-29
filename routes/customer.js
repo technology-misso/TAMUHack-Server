f63f5a9aae7d93ffe122dc7d79aee060ae2a7388
SharkBearGator-Laptop <47893402+SharkBearGator@users.noreply.github.com>
2022-01-29 15:59:29 -0600

    corrected db name and routes

--- routes/customer.js
+++ routes/customer.js
@@ -6,13 +6,28 @@ const databaseObject = require("../connection");
 const ObjectId = require("mongodb").ObjectId;
 
 customerRouter.route("/customers").get(function(request,result) {
+    console.log("yessir");
     let dbConnectToCollection = databaseObject.getDb();
     dbConnectToCollection.collection("Customers")
     .find({})
     .toArray(function (error, res) {
-        if (error) throw error;
+        if (error) console.log(error);
+        console.log(res);
         result.json(res);
     })
 });
+
+customerRouter.route("/customers/add").post(function(request, result) {
+    console.log("greetings");
+    let dbConnectToCollection = databaseObject.getDb();
+    let myObj = {
+        name:"boba",
+        points: 5
+    }
+    dbConnectToCollection.collection("Customers").insertOne(myObj, function(error, res) {
+        if(error) error;
+        result.json(res);
+    })
+})
 
 module.exports = customerRouter;
--- server.js
+++ server.js
@@ -8,6 +8,12 @@ server.use(cors());
 server.use(express.json());
 server.use(require("./routes/customer"));
 
+server.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); 
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+    
+});
 const databaseConnection = require("./connection");
  
 server.listen(portNumber, () => { databaseConnection.connectToServer(function (e) {
